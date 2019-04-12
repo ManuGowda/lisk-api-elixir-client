85b162f9ab49913732a79818bd034864c03224c8
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-04-12 12:06:55 +0200

    :seedling: Implement api client

--- lib/lisk_api_elixir_client.ex
+++ lib/lisk_api_elixir_client.ex
@@ -4,15 +4,81 @@ defmodule LiskApiElixirClient do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LiskApiElixirClient.hello()
-      :world
-
+  Lisk node address
   """
-  def hello do
-    :world
+  def node_address, do: Application.get_env(:lisk_api_elixir_client, :address)
+
+  @doc """
+  Function to construct the url
+  """
+  def build_url(route), do: "#{node_address()}/#{route}"
+
+  @doc """
+  Function to handle the get request
+  """
+  def handle_get_request(route, args) do
+    try do
+      case LiskApiElixirClient.Client.get(route, args) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body |> Poison.decode!()
+
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          "Invalid request :("
+
+        {:ok, %HTTPoison.Response{status_code: 400, body: body}} ->
+          body |> Poison.decode!()
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          reason
+      end
+    rescue
+      e -> e
+    end
+  end
+
+  @doc """
+  Function to handle the post request
+  """
+  def handle_post_request(route, args) do
+    try do
+      case LiskApiElixirClient.Client.post(route, args) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body |> Poison.decode!()
+
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          "Invalid request :("
+
+        {:ok, %HTTPoison.Response{status_code: _, body: body}} ->
+          body |> Poison.decode!()
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          reason
+      end
+    rescue
+      e -> e
+    end
+  end
+
+  @doc """
+  Function to handle the put request
+  """
+  def handle_put_request(route, args) do
+    try do
+      case LiskApiElixirClient.Client.put(route, args) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body |> Poison.decode!()
+
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          "Invalid request :("
+
+        {:ok, %HTTPoison.Response{status_code: _, body: body}} ->
+          body |> Poison.decode!()
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          reason
+      end
+    rescue
+      e -> e
+    end
   end
 end
