ab7a7199b5663f6ef6d30d67941c51c651a77d17
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-04-12 12:51:31 +0200

    :seedling: Implement blocks API

---
+++ lib/lisk_api_elixir_client/blocks.ex
@@ -0,0 +1,15 @@
+defmodule LiskApiElixirClient.Blocks do
+  @moduledoc """
+  Functions for retrieving information from Blocks.
+  """
+
+  @doc """
+  Get Lisk account.
+  **Method**: GET
+  LiskApiElixirClient.Blocks.get(%{address?: string, publicKey?: publicKey})
+  # => { data, meta, links }
+  """
+  def get(args) do
+    LiskApiElixirClient.handle_get_request('/blocks', args)
+  end
+end
